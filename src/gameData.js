51e4a948c6f4dc5d60000b8e689baab3477a723b
Peti <pmernyei@gmail.com>
2019-09-28 10:53:38 +0200

    Test state json stuff

--- src/gameData.js
+++ src/gameData.js
@@ -1,3 +1,26 @@
-export const gameData = { 
-    start: { text: "stuff"}
+export const gameData = {
+    start: {
+        text: "started",
+        actions: [{
+            text: "Do some stuff",
+            nextState: "stuffState"
+        }, {
+            text: "Do other stuff",
+            nextState: "otherStuffState"
+        }]
+    },
+    stuffState: {
+        text: "Did stuff",
+        actions: [{
+            text: "Go back",
+            nextState: "start"
+        }]
+    },
+    otherStuffState: {
+        text: "Did other stuff",
+        actions: [{
+            text: "Go back",
+            nextState: "start"
+        }]
+    }
 }
