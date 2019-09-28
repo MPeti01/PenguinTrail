735062c6a13a536ef74dbe36e5f178114ccce8f3
Peti <pmernyei@gmail.com>
2019-09-28 21:46:09 +0200

    Text almost done

--- src/gameData.js
+++ src/gameData.js
@@ -39,16 +39,20 @@ export const gameData = {
         text: "This place was ideal - now you need to find a place to nest to "+
               "bring your younglings to the world!",
         actions: [{
-            text: "Go to your usual nesting place near the coast",
+            text: "Go to your usual nesting place, a small cave near the plateau",
             nextState: "old1"
         },{
-            text: "new place",
+            text: "Look for a new place near the coast",
             nextState: "new1"
         }],
         lifes: 2
     },
     old1: {
-        text: "sad rabbit landslide",
+        text: "Oh, no! A landslide closed off the cave. You can't nest here anymore.",
+        infoDump: "Human often influence ecosystems in surprising ways by "+
+                  "introducing new animals. Grazing by rabbits introduced to "+
+                  "Macquarie Island has caused landslides that killed penguins "+
+                  "and destroyed nesting habitat.",
         actions: [{
             text: "new place",
             nextState: "new1"
@@ -56,114 +60,155 @@ export const gameData = {
         lifes: 1
     },
     new1: {
-        text: "cat!!",
+        text: "You found a promising place: there's a small hole in the "+
+              "guano which could easily be made into a larger burrow!",
         actions: [{
-            text: "look more",
+            text: "Take a look",
+            nextState: "new2"
+        }],
+        lifes: 2
+    },
+    new2: {
+        text: "A dangerous-looking wild cat is sleeping in the hole! You managed "+
+              "to leave quietly without waking it up",
+        infoDump: "Predation by introduced animals such as rats, cats, and "+
+                  "foxes also endangers Humboldt penguins.",
+        actions: [{
+            text: "Look for another new spot",
             nextState: "goodNest"
         },{
-            text: "old place",
+            text: "Take a look at your usual spot",
             nextState: "old2"
         }],
         lifes: 1
     },
     old2: {
-        text: "sad rabbit landslide",
+        text: "Oh, no! A landslide closed off the cave. You can't nest here anymore.",
+        infoDump: "Human often influence ecosystems in surprising ways by "+
+                  "introducing new animals. Grazing by rabbits introduced to "+
+                  "Macquarie Island has caused landslides that killed penguins "+
+                  "and destroyed nesting habitat.",
         actions: [{
-            text: "look for better new",
+            text: "Look for another new spot",
             nextState: "goodNest"
         }],
         lifes: 1
     },
     goodNest: {
-        text: "Found good nest",
+        text: "You finally found a sheltered spot where you could make a burrow!",
         actions: [{
-            text: "egg hatching",
+            text: "Prepare for the eggs hatching",
             nextState: "hatched"
         }],
         lifes: 2
     },
     hatched: {
-        text: "need to feed",
+        text: "Your chicks hatched! You take turns with your mate bringing them "+
+              "fresh food every day. Today it is your turn.",
         actions: [{
-            text: "go feed",
+            text: "Go find some fish",
             nextState: "fishing1"
         }],
         lifes: 2
     },
     fishing1: {
-        text: "chasing squid",
+        text: "While fishing with other penguins from the colony, you start "+
+              "chasing a really tasty-looking squid. It's trying to escape really "+
+              "hard!",
         actions: [{
-            text: "keep chasing",
+            text: "Keep chasing it",
             nextState: "bycatch"
         },{
-            text: "let go",
+            text: "Look for easier prey",
             nextState: "fishing2"
         }],
         lifes: 2
     },
     bycatch: {
-        text: "bycatch, dead",
+        text: "You were so focused on the squid that you didn't notice the humans " +
+              "fishing! They accidentally caught you in their net along with the " +
+              "fish they were after.",
+        infoDump: "Being caught in fishing nets is a serious danger to many penguin "+
+                  "species. Many fisheries don't make large efforts to avoid this "+
+                  "problem, but even mitigation devices aren't always successful.",
         actions: [{
-            text: "go back",
+            text: "Try choosing diffently",
             nextState: "fishing1"
         }],
         lifes: 0
     },
     fishing2: {
-        text: "going far",
+        text: "You're having trouble finding fish, the prey is unusually scarce. "+
+              "But you persevere for the sake of your chicks!",
         actions: [{
-            text: "go",
+            text: "Go on",
             nextState: "fishing3"
         }],
         lifes: 1
     },
     fishing3: {
-        text: "see something",
+        text: "You catch of glimpse of something shiny at the edge of your vision. "+
+              "Maybe some strange prey? What do you do?",
         actions: [{
-            text: "grab",
+            text: "Grab after it",
             nextState: "plastic"
         },{
-            text: "don't",
+            text: "Ignore it",
             nextState: "fishing4"
         }],
         lifes: 2
     },
     plastic: {
-        text: "plastic, dead",
+        text: "It turned out to be a piece of plastic - and you choked on it " +
+              "and died.",
+        infoDump: "Ocean pollution is a danger to penguins similarly to countless "+
+                  "aquatic species, who can easily get entangled in or swallow them.",
         actions: [{
-            text: "back",
+            text: "Try choosing differently",
             nextState: "fishing3"
         }],
         lifes: 0
     },
     fishing4: {
-        text: "really far, also storm coming",
+        text: "You're now going much farther than usual, and a big storm is starting "+
+              "to make your job even more difficult. But you keep going, you need "+
+              "to find food and there wasn't any closer to your home!",
+        infoDump: "Overfishing and competition from fisheries in general is a threat "+
+                  "to penguins indirectly: high amount of fish caught drastically reduce "+
+                  "the food availability for these penguins, forcing them to venture into "+
+                  "remoter hunting areas, implicitly increasing the dangers they have to face.",
         actions: [{
-            text: "keep going",
+            text: "Keep going",
             nextState: "fishing5"
         }],
         lifes: 1
     },
     fishing5: {
-        text: "finally catch",
+        text: "Finally, you caught a nice fat fish! You start to take it home to "+
+              "your family.",
         actions: [{
-            text: "take it home",
+            text: "Take it home",
             nextState: "goingHome"
         }],
         lifes: 2
     },
     goingHome: {
-        text: "long trek, torrential rain, must be very hungry",
+        text: "It's a long trek home, and now you're going through torrential rain. "+
+              "You've never seen such a big storm before. "+
+              "You try to hurry up, the younglings must be starving by now.",
         actions: [{
-            text: "keep going",
+            text: "Go to the nest",
             nextState: "arrived"
         }],
         lifes: 1
     },
     arrived: {
-        text: "Oh no, dead from rain",
+        text: "Oh, no! The little ones won't be eating the food you brought... "+
+              "They got soaked in the rain and died from hypothermia.",
+        infoDump: "The variability and extreme weather caused by climate change "+
+                  "is yet another danger putting penguins at the risk of extinction.",
         actions: [{
-            text: "So what can YOU do?",
+            text: "So what can WE do?",
             nextState: "lessonsLearned"
         }],
         lifes: 1
